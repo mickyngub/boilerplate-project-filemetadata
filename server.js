84e966bb82b6c4cf4e6e4a34addaf7468bb48008
mickyngub <micky47561@gmail.com>
2020-06-24 10:44:23 +0000

    upload a single file

--- server.js
+++ server.js
@@ -2,12 +2,28 @@
 
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
 
 // require and use "multer"...
+
+let storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './uploads')
+  }, filename : (req, file, cb) => {
+    cb(null, file.fieldname + '-' + Date.now());
+  } 
+})
+
+let upload = multer({storage : storage});
 
 var app = express();
 
 app.use(cors());
+app.use((req, res, done) => {
+  console.log(`${req.method} ${req.path} ${req.ip} `);
+  done();
+
+})
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
@@ -17,6 +33,17 @@ app.get('/', function (req, res) {
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
+  const file = req.file;
+  if(!file) {
+    const error = new Error('Please upload a file');
+    error.httpStatusCode = 400;
+    return next(error);
+  } else {
+    res.send(file);
+  }
+})
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
